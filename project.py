4a5174f17c54cc51729e5981cc61e85c76e4f033
Simone Ceriani <simone.ceriani@gmail.com>
2026-02-19 18:46:57 +0100

    use tar.gz for linux releases

--- project.py
+++ project.py
@@ -53,7 +53,7 @@ def checkout_func():
         repo_https_url + "cscosine",
         "3rdPartyBaseLibs",
         "eigen3",
-        "v0.1.0-rc1",
+        "v0.1.0-rc2",
         libs_os_presets,
     )
 
